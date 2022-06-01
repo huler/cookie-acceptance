faf6cb4bccc6f2d48dfdd4d6c80f939475057926
Chloe <chloe.baker@huler.io>
2022-06-01 09:29:17 +0100

    correcting a prop

--- src/Components/CookieAcceptance/CookieAcceptance.types.ts
+++ src/Components/CookieAcceptance/CookieAcceptance.types.ts
@@ -1,16 +1,15 @@
-import { CookieTypes } from '../../Helpers/Types';
+import { CookieTypes } from "../../Helpers/Types";
 
 export interface CookieAcceptanceProps {
-    smallText: string;
-    largeText;
-    string;
-    privacyPolicyURL: string;
-    image?: string;
-    injectScript: () => void;
-    onConfirm: () => void;
-    cookies: Array<CookieTypes>;
-    appName: string;
-    className: string;
-    toggleCookiePopup: () => void;
-    settings: boolean;
+  smallText: string;
+  largeText: string;
+  privacyPolicyURL: string;
+  image?: string;
+  injectScript: () => void;
+  onConfirm: () => void;
+  cookies: Array<CookieTypes>;
+  appName: string;
+  className: string;
+  toggleCookiePopup: () => void;
+  settings: boolean;
 }
